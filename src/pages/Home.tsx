354ab5910b54df066f6953ac06530c2303450a6c
David Ursueguía <david.ursueguia@gmail.com>
2025-03-06 12:20:05 +0100

    FEAT: Montar home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useCharacters } from "../hooks/useCharacters.ts";
+import Loading from "../components/Loading.tsx";
+import CharacterCard from "../components/CharacterCard.tsx";
+
 const Home = () => {
-  return (<h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>);
+  const { characters, loading, error } = useCharacters();
+
+  return (
+    <div>
+      {loading && <Loading />}
+      {error && <p className="text-center text-red-500">Error al cargar los personajes. Inténtalo más tarde.</p>}
+      {!loading && !error && characters.length === 0 && <p className="text-center">No hay personajes disponibles.</p>}
+      <div className="grid grid-cols-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7">
+        {characters.map((char) => (
+          <CharacterCard key={char.id} character={char} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
