174703010b6b935cb8002dadae7b47d747b7b5c0
David Ursueguía <david.ursueguia@gmail.com>
2025-03-07 22:50:15 +0100

    FIX: Improve animation on hover CharacterCard

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -21,25 +21,23 @@ const CharacterCard = ({ character }: { character: Character }) => {
         className="w-full h-64 object-cover"
       />
 
-      <div className="w-full h-1 bg-red-600"/>
-
-      <div className="relative w-full h-12 overflow-hidden">
+      <div className="w-full h-1 bg-red-600"></div>
+      <div className="relative w-full h-12 overflow-hidden bg-black">
         <div
-          className={`absolute  bottom-0 left-0 w-full h-full transition-all duration-300 ${
-            hovered ? "bg-red-600" : "bg-black"
-          }`}
+          className={`absolute top-0 left-0 w-full h-full transition-transform duration-150 transform ${
+            hovered ? "scale-y-100 bg-red-600" : "scale-y-0 bg-red-600"
+          } origin-top`}
         ></div>
 
-        <div className="absolute left-0 w-full flex items-center justify-between px-2 py-2 z-10">
+        <div className="absolute bottom-0 left-0 w-full flex items-center justify-between px-2 py-2 z-10">
           <p className="text-white text-sm uppercase">{character.name}</p>
-
-          <HeartButton character={character} hovered={hovered} />
+          <HeartButton character={character} hovered={hovered}/>
         </div>
 
         <img
           src={cut}
           alt="cut"
-          className="filter fill-current absolute bottom-[-1px] right-[-1px] h-4 w-4 z-20"
+          className="filter absolute bottom-[-1px] right-[-1px] h-4 w-4 z-20"
         />
       </div>
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.5s ease-in-out forwards'
+      }
+    },
+  },
+  plugins: [],
+}
