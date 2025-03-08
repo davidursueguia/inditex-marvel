3cbf4c1b64a748f13da2471cc9b01397e0e91330
David Ursueguía <david.ursueguia@gmail.com>
2025-03-08 22:53:43 +0100

    REFACTOR: Favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -8,28 +8,22 @@ const Favorites = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const filteredFavorites = favorites.filter((char) =>
-    char.name.toLowerCase().includes(searchQuery.toLowerCase())
+    char.name.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
   return (
     <div className="pl-12 pr-12 pt-12">
       <p className="float-left text-2xl font-bold leading-7">FAVORITES</p>
-
       <SearchBar
         onSearch={setSearchQuery}
         resultsCount={filteredFavorites.length}
         isLoading={false}
       />
-
-      {filteredFavorites.length === 0 ? (
-        <p className="text-center text-gray-500 mt-6">No favorites found.</p>
-      ) : (
-        <div className="grid grid-cols-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4 mt-6">
-          {filteredFavorites.map((char) => (
-            <CharacterCard key={char.id} character={char} />
-          ))}
-        </div>
-      )}
+      <div className="grid grid-cols-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4 mt-6">
+        {filteredFavorites.map((char) => (
+          <CharacterCard key={char.id} character={char} />
+        ))}
+      </div>
     </div>
   );
 };
