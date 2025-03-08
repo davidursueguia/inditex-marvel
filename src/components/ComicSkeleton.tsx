d1fa25802dc0db34d218f761d4e7c6ee021d0782
David Ursueguía <david.ursueguia@gmail.com>
2025-03-08 22:39:09 +0100

    FIX: Improve skeletons

--- src/components/ComicSkeleton.tsx
+++ src/components/ComicSkeleton.tsx
@@ -1,8 +1,7 @@
 const ComicSkeleton = () => {
   return <div className="w-44 flex-shrink-0 animate-pulse">
     <div className="h-64 bg-gray-300 rounded-lg"></div>
-    <div className="h-4 bg-gray-300 rounded mt-3 w-3/4"></div>
-    <div className="h-3 bg-gray-300 rounded mt-2 w-1/2"></div>
+    <div className="h-4 bg-gray-300 rounded mt-3"></div>
   </div>;
 };
 export default ComicSkeleton;
