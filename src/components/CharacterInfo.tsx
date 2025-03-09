ce33588fae1a5ad6f950eef98486a652345552c1
David Ursueguía <david.ursueguia@gmail.com>
2025-03-09 15:32:11 +0100

    FIX: Responsive on CharacterInfo WIP

--- src/components/CharacterInfo.tsx
+++ src/components/CharacterInfo.tsx
@@ -1,19 +1,34 @@
-import HeartButton from "./HeartButton";
-import { Character } from "../types/characterTypes";
+import { Character } from "../types/characterTypes.ts";
+import HeartButton from "./HeartButton.tsx";
+import cut from "../assets/cut.svg";
 
 interface CharacterInfoProps {
   character: Character;
 }
 
 const CharacterInfo = ({ character }: CharacterInfoProps) => (
-  <div className="mt-4 lg:mt-0 lg:ml-8 flex flex-col justify-center w-full max-w-[640px]">
-    <div className="flex w-full justify-between">
-      <h1 className="text-2xl sm:text-3xl font-bold">{character.name.toUpperCase()}</h1>
-      <HeartButton character={character} />
+  <div className="bg-black flex flex-col md:!flex-row lg:!flex-row w-full justify-self-center text-white px-4 lg:px-0">
+    <div className="w-full lg:w-64 h-64 overflow-hidden rounded-lg shadow-lg mb-4 lg:mb-0 max-w-[960px]">
+      <img
+        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+        alt={character.name}
+        className="w-full h-full object-cover sm:!h-[500px] sm:!w-full"
+      />
     </div>
-    <p className="mt-2 text-gray-300 text-sm sm:text-base text-left">
-      {character.description || "No description available."}
-    </p>
+    <div className="lg:ml-8 flex flex-col justify-center w-full">
+      <div className="flex md:ml-11 ml-7 justify-between">
+        <h1 className="text-2xl sm:text-3xl font-bold">{character.name.toUpperCase()}</h1>
+        <HeartButton character={character} />
+      </div>
+      <p className="mt-2 text-gray-300 text-sm sm:text-base sm:text-start sm:ml-7 sm:pb-9">
+        {character.description || "No description available."}
+      </p>
+    </div>
+    <img
+      src={cut}
+      alt="cut"
+      className="sticky right-0 bottom-0 self-end mb-[-1px] mr-[-1px]"
+    />
   </div>
 );
 
--- src/pages/CharacterDetail.tsx
+++ src/pages/CharacterDetail.tsx
@@ -4,7 +4,6 @@ import { useCharacterById } from "../hooks/useCharacterById.ts";
 import Loading from "../components/Loading.tsx";
 import { useComicsByCharacterId } from "../hooks/useComics.ts";
 import ErrorMessage from "../components/ErrorMessage.tsx";
-import cut from "../assets/cut.svg";
 import CharacterInfo from "../components/CharacterInfo.tsx";
 
 const CharacterDetail = () => {
@@ -17,21 +16,7 @@ const CharacterDetail = () => {
 
   return (
     <div>
-      <div className="bg-black text-white flex flex-col lg:flex-row items-center lg:items-start justify-center h-auto lg:h-[320px] w-full px-4 sm:px-8 lg:px-16 relative py-8 lg:py-0">
-        <div className="w-full sm:w-64 h-64 overflow-hidden rounded-lg shadow-lg">
-          <img
-            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-            alt={character.name}
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <CharacterInfo character={character} />
-        <img
-          className="h-6 w-6 absolute bottom-[-3px] right-[-3px]"
-          src={cut}
-          alt={"cut"}
-        />
-      </div>
+      <CharacterInfo character={character} />
       <ComicCarrousel
         comics={comics}
         isLoading={comicsAreLoading}
